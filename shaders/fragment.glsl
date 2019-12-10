7ba7f3222360f18ba546efc9e54ba9aca152b2c8
mthom26 <mthom2626@gmail.com>
2019-12-10 12:38:03 +0000

    01 Getting Started - 02 Hello Triangle

---
+++ shaders/fragment.glsl
@@ -0,0 +1,5 @@
+out vec3 frag_color;
+
+void main() {
+  frag_color = vec3(0.9, 0.5, 0.1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use luminance::context::GraphicsContext;
+use luminance::{
+    context::GraphicsContext,
+    render_state::RenderState,
+    shader::program::Program,
+    tess::{Mode, TessBuilder},
+};
+use luminance_derive::{Semantics, Vertex};
 use luminance_glutin::{
     ElementState, Event, GlutinSurface, KeyboardInput, Surface, VirtualKeyCode, WindowDim,
     WindowEvent, WindowOpt,
@@ -6,6 +12,32 @@ use luminance_glutin::{
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
+
+// Shaders
+const VS: &str = include_str!("../shaders/vertex.glsl");
+const FS: &str = include_str!("../shaders/fragment.glsl");
+
+#[derive(Copy, Clone, Debug, Semantics)]
+pub enum Semantics {
+    #[sem(name = "position", repr = "[f32; 3]", wrapper = "VertexPos")]
+    Position,
+}
+
+#[repr(C)]
+#[derive(Vertex)]
+#[vertex(sem = "Semantics")]
+struct Vertex {
+    pos: VertexPos,
+}
+
+impl Vertex {
+    // Convenience function to build a Vertex
+    fn from(pos: [f32; 3]) -> Self {
+        Vertex {
+            pos: VertexPos::new(pos),
+        }
+    }
+}
 
 fn main() {
     let mut surface = GlutinSurface::new(
@@ -16,6 +48,24 @@ fn main() {
     .expect("Could not create GLUTIN surface.");
 
     let back_buffer = surface.back_buffer().unwrap();
+
+    // Shader Program
+    let program: Program<Semantics, (), ()> = Program::from_strings(None, VS, None, FS)
+        .unwrap()
+        .ignore_warnings();
+
+    // Triangle
+    let vertices = [
+        Vertex::from([0.0, 0.5, 0.0]),
+        Vertex::from([0.5, -0.5, 0.0]),
+        Vertex::from([-0.5, -0.5, 0.0]),
+    ];
+
+    let triangle = TessBuilder::new(&mut surface)
+        .add_vertices(vertices)
+        .set_mode(Mode::Triangle)
+        .build()
+        .unwrap();
 
     'app: loop {
         // Handle Input
@@ -44,7 +94,13 @@ fn main() {
 
         surface
             .pipeline_builder()
-            .pipeline(&back_buffer, clear_color, |_, _| {});
+            .pipeline(&back_buffer, clear_color, |_, mut shd_gate| {
+                shd_gate.shade(&program, |_, mut rdr_gate| {
+                    rdr_gate.render(RenderState::default(), |mut tess_gate| {
+                        tess_gate.render(&triangle);
+                    });
+                });
+            });
 
         surface.swap_buffers();
     }
